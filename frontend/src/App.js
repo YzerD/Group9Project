758eb461f7c3689164b8d15e695fae0a3e1ab235
Proggalabonikundu <laboniprogga34@gmail.com>
2023-12-16 22:45:35 -0500

    new

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,6 @@
+// src/App.js
 import React, { useState } from 'react';
+import axios from 'axios';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Header from './components/Header/Header';
 import SearchBar from './components/SearchBar/SearchBar';
@@ -10,14 +12,44 @@ import Random from './components/Random/Random';
 import RandomButton from './components/RandomButton/RandomButton';
 import FilterButton from './components/FilterButton/FilterButton';
 import FilterResult from './components/FilterResult/FilterResult';
+import Signup from './components/Auth/Signup';
+import Login from './components/Auth/Login';
+import ForgotPassword from './components/Auth/ForgotPassword';
 
-const App = () => {
+function App() {
   const [searchFilters, setSearchFilters] = useState({});
   const [filterParams, setFilterParams] = useState({});
+  const [signupData, setSignupData] = useState({
+    username: '',
+    email: '',
+    password: '',
+  });
+  const [loginData, setLoginData] = useState({
+    email: '',
+    password: '',
+  });
 
   const applyFilters = (filters) => {
     setSearchFilters(filters.results);
     setFilterParams({ ...filters, apiUrl: null });
+  };
+
+  const handleSignup = async () => {
+    try {
+      const response = await axios.post('http://localhost:5000/signup', signupData);
+      console.log(response.data);
+    } catch (error) {
+      console.error(error.response.data);
+    }
+  };
+
+  const handleLogin = async () => {
+    try {
+      const response = await axios.post('http://localhost:5000/login', loginData);
+      console.log(response.data);
+    } catch (error) {
+      console.error(error.response.data);
+    }
   };
 
   return (
@@ -62,10 +94,32 @@ const App = () => {
           />
           {/* Random Movie Generator page */}
           <Route path='/random' element={<Random />} />
+          {/* Authentication routes */}
+          <Route
+            path='/signup'
+            element={
+              <Signup
+                signupData={signupData}
+                setSignupData={setSignupData}
+                handleSignup={handleSignup}
+              />
+            }
+          />
+          <Route
+            path='/login'
+            element={
+              <Login
+                loginData={loginData}
+                setLoginData={setLoginData}
+                handleLogin={handleLogin}
+              />
+            }
+          />
+          <Route path='/forgot-password' element={<ForgotPassword />} />
         </Routes>
       </div>
     </Router>
   );
-};
+}
 
 export default App;
