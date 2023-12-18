400a7e5fd2614b1105d70709eb143063fd679159
YzerD <yzerdegula@gmail.com>
2023-12-18 02:39:26 -0500

    bababooey

--- backend/server.js
+++ backend/server.js
@@ -11,6 +11,8 @@ require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
 
+app.use(express.json());
+
 // MongoDB Connection
 const mongoUri = process.env.MONGODB_URI || 'mongodb://localhost:27017/auth';
 mongoose.connect(mongoUri, {
