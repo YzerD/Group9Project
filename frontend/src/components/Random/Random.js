145850bfee40df6232d7a1f3d3c995abf52eeaf4
YzerD <yzerdegula@gmail.com>
2023-12-18 01:56:13 -0500

    I never believed in God, but boy do I do now

--- frontend/src/components/Random/Random.js
+++ frontend/src/components/Random/Random.js
@@ -1,10 +1,9 @@
 import React, { useState } from 'react';
-import { Link, useNavigate } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import genresData from '../genres/genres';
 import './Random.css';
 
 const Random = () => {
-  const history = useNavigate();
   const [selectedGenres, setSelectedGenres] = useState([]);
   const [selectedGenreDropdown, setSelectedGenreDropdown] = useState("all");
   const [remainingGenres, setRemainingGenres] = useState(genresData);
